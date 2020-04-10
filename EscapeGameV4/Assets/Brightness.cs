cfbddc5c87c03f153e9fab006769acc83cd4edc8
tibo29450 <t7rollan@enib.fr>
2020-04-10 14:57:12 +0200

    luminositeSettings terminée

--- EscapeGameV4/Assets/Brightness.cs
+++ EscapeGameV4/Assets/Brightness.cs
@@ -5,22 +5,70 @@ using UnityEngine.UI;
 public class Brightness : MonoBehaviour
 {
 
-    public float GammaCorrection=0.5f;
+    public float GammaCorrection =0.5f;
+
+    //enregistrement des donnée
+    private static readonly string FirstPlayPref2 = "FirstPlayPref2";
+    private int firstPlayInt;
+    private static readonly string LuminosityPref = "LuminosityPref";
 
     public Slider luminosity;
 
-    void Update()
+    void Start()
     {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlayPref2);
 
-        RenderSettings.ambientLight = new Color(GammaCorrection, GammaCorrection, GammaCorrection, 1.0f);
-
+        if (firstPlayInt == 0)
+        {
+            print("test");
+            luminosity.value = GammaCorrection;
+            PlayerPrefs.SetFloat(LuminosityPref, GammaCorrection);
+            PlayerPrefs.SetInt(FirstPlayPref2, -1);
+        }
+        else
+        {
+            print("else");
+            GammaCorrection = PlayerPrefs.GetFloat(LuminosityPref);
+            RenderSettings.ambientLight = new Color(GammaCorrection, GammaCorrection, GammaCorrection, 1.0f);
+            luminosity.value = GammaCorrection;
+        }
     }
 
-    void OnGUI()
-    {
 
-        GammaCorrection = luminosity.value;
-        print(GammaCorrection);
+
+
+
+
+        
+        
+
+    public void setLuminosity(float sliderValue)
+    {
+            
+       RenderSettings.ambientLight = new Color(sliderValue, sliderValue, sliderValue, 1.0f);
+            
+    }
+
+
+
+
+    // Fonction qui sauvegarde dans playerPrefs les valeurs des sliders
+    public void SaveLuminositySettings()
+    {
+        PlayerPrefs.SetFloat(LuminosityPref, luminosity.value);
+        print("SaveLuminositySettings appelé");
+        print(PlayerPrefs.GetFloat(LuminosityPref));
+    }
+
+
+    // Quand on quitte l'application, on appelle la fonction qui sauvegarde les données du slider
+    void OnApplicationFocus(bool inFocus)
+    {
+        if (!inFocus)
+        {
+            SaveLuminositySettings();
+        }
     }
 
 }
+

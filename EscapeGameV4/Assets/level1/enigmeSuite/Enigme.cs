4a4eb3a66d53d0f8011f0c5bcfa7ab44e1ff1604
tibo29450 <t7rollan@enib.fr>
2020-04-17 11:47:55 +0200

    debut script enigme

---
+++ EscapeGameV4/Assets/level1/enigmeSuite/Enigme.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Enigme : MonoBehaviour
+{
+   
+
+
+    void start()
+    {
+
+        List<int> liInt = new List<int>();
+        liInt.Add(2);
+        liInt.Add(3);
+        print(liInt);
+
+
+
+
+    }
+
+
+
+
+}

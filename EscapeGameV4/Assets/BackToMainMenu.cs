4d7cfd27a97ce707227dd6282db1835b532d5033
tibo29450 <t7rollan@enib.fr>
2020-04-20 15:47:02 +0200

    je sais pas

---
+++ EscapeGameV4/Assets/BackToMainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackToMainMenu : MonoBehaviour
+{
+  
+    void GoBackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+}

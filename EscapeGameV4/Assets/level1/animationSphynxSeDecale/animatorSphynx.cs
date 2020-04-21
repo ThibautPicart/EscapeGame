6418123f83f1d9aa7404854cb4880a85806c398a
tibo29450 <t7rollan@enib.fr>
2020-04-21 10:59:56 +0200

    avancement interaction mur

--- EscapeGameV4/Assets/level1/animationSphynxSeDecale/animatorSphynx.cs
+++ EscapeGameV4/Assets/level1/animationSphynxSeDecale/animatorSphynx.cs
@@ -1,20 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class animatorSphynx : MonoBehaviour
 {
+    //pour récuperer la solution associée à la suite qui a été tirée au sort dans le script enigme
+    private static readonly string solutionSuite = "solutionSuite";
     private Animator anim;
+
+    //liste des images du murs pour voir si le joueur a cliqué dessus
+    public Image zeroMur;
+    public Image unMur;
+    public Image deuxMur;
+    public Image troisMur;
+    public Image quatreMur;
+    public Image cinqMur;
+    public Image sixMur;
+    public Image septMur;
+    public Image huitMur;
+    public Image neufMur;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        string Solution = PlayerPrefs.GetString(solutionSuite);
+        print("la solution a la suite est : " + Solution);
+        int nombreChiffreSolution = Solution.Length;
+
+        //on va séparer caque chiffre du string en char 
+        List<char> charList = new List<char>();
+        for(int i=0; i < nombreChiffreSolution; ++i)
+        {
+            charList.Add(Solution[i]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))//a terme il faudra mettre si solution = true
+        if (1==2)
         {
             anim.SetBool("SphynxMove", true);
         }

7115b7b50f3ebafaca94fea4aaced52deb1e417d
Thibaut Picart <t7picart@enib.fr>
2020-04-23 10:10:52 +0200

    inventaire fonctionnel

---
+++ EscapeGameV4/Assets/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject InventoryCanvas;
+
+
+
+    void Start()
+    {
+        PlayerPrefs.SetInt("EmplacementDispoPref", 1); // On initialise le premier emplacement dispo de l'inventaire
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            InventoryCanvas.SetActive(!InventoryCanvas.activeSelf);
+        }
+    }
+}

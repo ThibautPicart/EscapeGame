1ecdde2d29cf1a8f0e367120efd72be16c3cef3f
tibo29450 <t7rollan@enib.fr>
2020-04-30 22:39:13 +0200

    last commit

---
+++ EscapeGameV4/Assets/level1/equationsMur/equations.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class equations : MonoBehaviour
+{
+
+    public Text equation;
+
+
+    //equation niveau facile sont des droites f(x)= ax+b
+
+    //on donne en premier le coefficient directeur a et en seconde position l'ordonée à l'origine b
+    //float[] droite1 = new float { 1, 0 };
+    //float[] droite2 = new float { 2, 0 };
+
+
+    //intermédiaire cercle
+
+    //difficile elypse
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        equation.text = "bonjou";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

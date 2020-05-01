ddf3cc2a0c125e0b2f55763489923c64f59f6bce
Thibaut Picart <t7picart@enib.fr>
2020-05-01 11:33:17 +0200

    presque fini

--- EscapeGameV4/Assets/Inventory.cs
+++ EscapeGameV4/Assets/Inventory.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject InventoryCanvas;
+    public GameObject HelpPopup;
+    public GameObject camera2;
+    public InputField inputField;
+
+    //liste des items que l'on peut ramasser
+    public GameObject tablette;
+    public GameObject stick;
+
+    //Le joueur
+    public GameObject player;
+
+    //Déclaration de la liste de ces items
+    public List<GameObject> items;
+
+    public int firstTime;
+
+
     void Start()
     {
-        
+        PlayerPrefs.SetInt("EmplacementDispoPref", 1); // On initialise le premier emplacement dispo de l'inventaire
+        items = new List<GameObject>() { tablette, stick };
+        firstTime = 0;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("i") && inputField.isFocused == false)
+        {
+            InventoryCanvas.SetActive(!InventoryCanvas.activeSelf);
+            camera2.SetActive(!camera2.activeSelf);
+            player.SetActive(!player.activeSelf);
+        }
+
+        foreach (GameObject item in items)
+        {
+            if ((Vector3.Distance(item.transform.position, player.transform.position)) < 2 && firstTime == 0)
+            {
+                //On affiche le conseil
+                HelpPopup.SetActive(true);
+                firstTime = -1;
+            }
+        }
+
     }
 }
+

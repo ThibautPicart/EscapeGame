7ccb4143ff61bddeb5fe0662ee2273c5749633ec
tibo29450 <t7rollan@enib.fr>
2020-05-02 11:03:11 +0200

    avancement equation

--- EscapeGameV4/Assets/level1/equationsMur/equations.cs
+++ EscapeGameV4/Assets/level1/equationsMur/equations.cs
@@ -20,74 +20,108 @@ public class equations : MonoBehaviour
     //on donne en premier le coefficient directeur a et en seconde position l'ordonée à l'origine b
     //en troisieme et quatrieme position, coefficient directeur et ordonnée à m'origine de la deuxième droite
 
-    List<string> droite1 = new List<string>() { "1","0","-1","0" };
-    List<string> droite2 = new List<string>() { "1", "0" ,"-2","0"};
+    private List<string> droite1 = new List<string>() { "1","0","-1","0" };
+    private List<string> droite2 = new List<string>() { "3", "0" ,"-2","0"};
 
 
-    //intermédiaire cercle
-    List<string> cercleDroite1 = new List<string>() { "1", "0", "-1", "0" };
-    List<string> cercleDroite2 = new List<string>() { "1", "0", "-2", "0" };
+    ////intermédiaire cercle
+    //les deux premier nombre sont le coefficient directeur et l'ordonée à l'origine de la droite
+    //les trois dernier nombre sont : coordonnée du centre du cercle et le rayon du cercle au carré
+    private List<string> cercleDroite1 = new List<string>() { "1", "0", "0", "0","9" };
+    private List<string> cercleDroite2 = new List<string>() { "2", "1", "1", "2","16" };
 
-    //difficile elypse
-    List<string> cercleElyspe1 = new List<string>() { "1", "0", "-1", "0" };
-    List<string> cercleElypse2 = new List<string>() { "1", "0", "-2", "0" };
+    ////difficile elypse
+    private List<string> cercleElyspe1 = new List<string>() { "1", "0", "-1", "0" };
+    private List<string> cercleElypse2 = new List<string>() { "1", "0", "-2", "0" };
 
 
 
-    //on fait les listes composées de toutes les equations
-    List<List<string>> equationsFaciles = new List<List<string>>() { droite1, droite2 };
-    List<List<string>> equationsIntermediaires = new List<List<string>>() { cercleDroite1, cercleDroite2 };
-    List<List<string>> equationsDifficiles = new List<List<string>>() { cercleElyspe1, cercleElyspe2 };
-
-    List<string> equationChosen;
+    private List<string> equationChosen;
  
     //public string solutionChosen;
 
     // Start is called before the first frame update
     void Start()
     {
-        //équation1 ce qu'on ecrit sur le mur
-        TMP_Text equation1Text = equation1.GetComponent<TMP_Text>();
-        equation1Text.SetText("f(x)= ");
-        //équation2
-        TMP_Text equation2Text = equation2.GetComponent<TMP_Text>();
-        equation2Text.SetText("g(x)=");
+
+    //on fait les listes composées de toutes les equations: obligé de mettre ça dans le start !
+     List<List<string>> equationsFaciles = new List<List<string>>() { droite1, droite2 };
+     List<List<string>> equationsIntermediaires = new List<List<string>>() { cercleDroite1, cercleDroite2 };
+     List<List<string>> equationsDifficiles = new List<List<string>>() { cercleElyspe1, cercleElypse2 };
 
 
-        ////on récupère le niveau selectionné
-        //levelString = PlayerPrefs.GetString(levelDifficulty);
-        //print(levelString);
 
-        ////niveau facil
-        //if (levelString == "Easy")
-        //{
-        //    int randomInt = Random.Range(0, equationsFaciles.Count); //Génère un entier compris entre 0 et le nombre d'élément de la liste
 
-        //    //on va donc selectionner les equations maintenant
-        //    equationChosen = equationsFaciles[randomInt];
 
-           //je selectionne aussi la bonne réponse correpondante afin de l'envoyer danns le script qui fait bouger le sphynx
-          
-           //S SolutionChosen = Solution;
-        //}
+        
 
-        //else if (levelString == "intermediate")
-        //{
-        //    print("jbnk");
-        //}
 
-        //else//if(levelString == "Difficult")
-        //{
-        //    print("jbnk");
-        //}
+        //on récupère le niveau selectionné
+        levelString = PlayerPrefs.GetString(levelDifficulty);
+        //print(levelstring);
+
+        //niveau facil
+        if (levelString == "Easy")
+        {
+            int randomInt = Random.Range(0, equationsFaciles.Count); //génère un entier compris entre 0 et le nombre d'élément de la liste
+
+            //on va donc selectionner les equations correspondantes maintenant
+            equationChosen = equationsFaciles[randomInt];
+
+            //je selectionne aussi la bonne réponse correpondante afin de l'envoyer danns le script qui fait bouger le sphynx
+
+
+
+            //maintenant on va écrire sur le mur les équation correcpondante
+            //équation1 ce qu'on ecrit sur le mur
+            TMP_Text equation1Text = equation1.GetComponent<TMP_Text>();
+            equation1Text.SetText("f(x)= " + equationChosen[0] + "x +" + equationChosen[1]);
+            //équation2
+            TMP_Text equation2Text = equation2.GetComponent<TMP_Text>();
+            equation2Text.SetText("g(x)=" + equationChosen[2] + "x +" + equationChosen[3]);
+
+        }
+
+        else if (levelString == "Intermediate")
+        {
+            int randomInt = Random.Range(0, equationsIntermediaires.Count); //génère un entier compris entre 0 et le nombre d'élément de la liste
+
+            //on va donc selectionner les equations correspondantes maintenant
+            equationChosen = equationsIntermediaires[randomInt];
+
+            //je selectionne aussi la bonne réponse correpondante afin de l'envoyer danns le script qui fait bouger le sphynx
+            print("les équation choisies sont :\n" + equationChosen[0] + "x + " + equationChosen[1]);
+            //maintenant on va écrire sur le mur les équation correcpondante
+            //maintenant on va écrire sur le mur les équation correcpondante
+            //équation1 ce qu'on ecrit sur le mur
+            TMP_Text equation1Text = equation1.GetComponent<TMP_Text>();
+            equation1Text.SetText("f(x)= " + equationChosen[0] + "x +" + equationChosen[1]);//la droite
+            //équation2
+            TMP_Text equation2Text = equation2.GetComponent<TMP_Text>();
+            equation2Text.SetText("(x-" + equationChosen[2] + ")^2 + (y-" + equationChosen[3] + ")^2=" + equationChosen[4]);//le cercle
+        }
+
+        else//if(levelstring == "difficult")
+        {
+            int randomInt = Random.Range(0, equationsDifficiles.Count); //génère un entier compris entre 0 et le nombre d'élément de la liste
+
+            //on va donc selectionner les equations correspondantes maintenant
+            equationChosen = equationsDifficiles[randomInt];
+
+            //je selectionne aussi la bonne réponse correpondante afin de l'envoyer danns le script qui fait bouger le sphynx
+            print("les équation choisies sont :\n" + equationChosen[0] + "x + " + equationChosen[1]);
+            //maintenant on va écrire sur le mur les équation correcpondante
+        }
+
+
 
 
     }
 
 
 
-    // Update is called once per frame
-    void Update()
+// Update is called once per frame
+void Update()
     {
         
     }

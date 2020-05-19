36cef415740d16e5a07b9cc2f3df2854f2a68b7b
Thibaut Picart <t7picart@enib.fr>
2020-05-19 17:41:04 +0200

    ok

---
+++ EscapeGameV4/Assets/level1/persoTest/camController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class camController : MonoBehaviour
+{
+    public Transform perso;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    public float rotationSensi = 0.125f;
+    private Vector3 prevPos;
+
+    private float deltaX;
+    private float deltaY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(0, 1.5f, 0);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = perso.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, rotationSensi, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -rotationSensi, 0);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(rotationSensi, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(-rotationSensi, 0, 0);
+        }
+
+        deltaX = prevPos.x - Input.mousePosition.x;
+        deltaY = prevPos.y - Input.mousePosition.y;
+
+        if(Input.GetMouseButton(1))
+        {
+            transform.Rotate(deltaY*rotationSensi, -deltaX*rotationSensi, 0);
+        }
+
+
+        prevPos = Input.mousePosition;
+
+    }
+}
--- EscapeGameV4/Assets/level1/persoTest/persoController.cs
+++ EscapeGameV4/Assets/level1/persoTest/persoController.cs
@@ -2,77 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class persoController : MonoBehaviour
 {
-    private Animator personnageAnim;
-    //on definit la gravité pour faire redescendre le personnage apres un saut plus ou moins rapidement
-    int gravite = 20;
-    //on crée un un float qui va gérer la vitesse de déplacement de notre personnage
-    public float speed = 3;
-    //vitesse de deplacement lorsque le personnage court
-    public float runSpeed = 6;
-    //pour régler la hauteur du saut
-    public int jump = 5;
-    private Vector3 DirectionalDeplacement = Vector3.zero;
     private CharacterController Player;
-    //on va faire tourner le personnage à l'aide de la sourie; on va donc créer une variable pour changer la sensiblité
-    public float rotationSensi=2;
-    //pour glisser la camera
-    //public Camera camera;
-
-    public float speedV = 2.0f;
-    private float pitch = 0.0f;
-
-
+    private Animator personnageAnim;
+    private Vector3 DirectionalDeplacement = Vector3.zero;
+    public float speed = 3;
+    public float runSpeed = 6;
+    public int jump = 5;
+    int gravite = 20;
+    public float rotationSensi = 4;
     // Start is called before the first frame update
     void Start()
     {
-        //on récupère le personnage controller
         Player = GetComponent<CharacterController>();
-        //on récupere l'animator du perso
         personnageAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //pitch -= speedV * Input.GetAxis("Mouse Y");
-        //camera.transform.eulerAngles.Set(pitch, camera.transform.eulerAngles.y, camera.transform.eulerAngles.z);
-
-        //on va donc faire évoluer les coordonées du personnage
         DirectionalDeplacement.z = Input.GetAxisRaw("Vertical");
         DirectionalDeplacement.x = Input.GetAxisRaw("Horizontal");
-        //pour le déplacement du personnage
         DirectionalDeplacement = transform.TransformDirection(DirectionalDeplacement);
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             Player.Move(DirectionalDeplacement * Time.deltaTime * runSpeed);
         }
-        else 
+        else
         {
             Player.Move(DirectionalDeplacement * Time.deltaTime * speed);
         }
 
-        //pour faire tourner le personnage sur lui mmeme avec q et d 
-        if (Input.GetKey("d")|| Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey("d"))
         {
             transform.Rotate(0, rotationSensi, 0);
         }
 
-        if (Input.GetKey("q")||Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey("q"))
         {
             transform.Rotate(0, -rotationSensi, 0);
         }
 
-
-
-        //saut du personnage quand on appui sur espace
-
-        if (Input.GetKeyDown(KeyCode.Space ) && Player.isGrounded)//&& Player.isGrounded
+        if (Input.GetKeyDown(KeyCode.Space) && Player.isGrounded)//&& Player.isGrounded
         {
             personnageAnim.SetBool("jump", true);
             DirectionalDeplacement.y = jump;
-            
+
         }
 
         if (!Player.isGrounded)
@@ -81,19 +58,19 @@ public class playerController : MonoBehaviour
             DirectionalDeplacement.y -= gravite * Time.deltaTime;
         }
 
-        //pour les animation
+        //pour les animations
 
         //pour marcher
-        if ((Input.GetKey(KeyCode.Z)|| Input.GetKey(KeyCode.UpArrow))&& !Input.GetKey(KeyCode.LeftShift))
+        if ((Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow)) && !Input.GetKey(KeyCode.LeftShift))
         {
-            personnageAnim.SetBool("walk",true);
+            personnageAnim.SetBool("walk", true);
         }
         if (!Input.GetKey(KeyCode.Z) && !Input.GetKey(KeyCode.UpArrow))
         {
             personnageAnim.SetBool("walk", false);
         }
 
-        //0pour courir
+        //pour courir
         if ((Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.UpArrow)) && Input.GetKey(KeyCode.LeftShift))
         {
             personnageAnim.SetBool("run", true);
@@ -103,6 +80,5 @@ public class playerController : MonoBehaviour
         {
             personnageAnim.SetBool("run", false);
         }
-
     }
 }
